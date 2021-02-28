3c3daa5e14274c73c369fb4aba02706813de0ec1
seouuuukjin <uukjin4@gmail.com>
2021-02-28 19:13:45 +0900

    firebase import 및 export

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import * as firebase from "firebase/app";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBJc0EvQtixVoeB1Q1I00Ey4acTkBphfXA",
     authDomain: "tutorial-tweet.firebaseapp.com",
@@ -6,3 +9,5 @@ const firebaseConfig = {
     messagingSenderId: "455593286823",
     appId: "1:455593286823:web:0ece66ea784cf633728f82"
   };
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
